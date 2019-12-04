013930dcc3a04470b1c78e177a152fa1527bf76d
ExpandingMan <savastio@protonmail.com>
2019-12-04 14:18:49 -0500

    added export of s3_list_objects_v2

--- src/AWSS3.jl
+++ src/AWSS3.jl
@@ -16,7 +16,7 @@ export S3Path, s3_arn, s3_put, s3_get, s3_get_file, s3_exists, s3_delete, s3_cop
        s3_create_bucket,
        s3_put_cors,
        s3_enable_versioning, s3_delete_bucket, s3_list_buckets,
-       s3_list_objects, s3_list_keys, s3_list_versions,
+       s3_list_objects, s3_list_objects_v2, s3_list_keys, s3_list_versions,
        s3_get_meta, s3_purge_versions,
        s3_sign_url, s3_begin_multipart_upload, s3_upload_part,
        s3_complete_multipart_upload, s3_multipart_upload,
