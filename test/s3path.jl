db0c1643e004ac67dda70cc5bbd30ae8b44a6f75
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-06-25 16:57:14 +0200

    Update test/s3path.jl

--- test/s3path.jl
+++ test/s3path.jl
@@ -337,6 +337,9 @@ end
 @testset "JSON roundtripping" begin
     json_path = S3Path("s3://$(bucket_name)/test_json"; config=aws)
     my_dict = Dict("key" => "value", "key2" => 5.0)
+    # here we use the "application/json" MIME type to trigger the heuristic parsing into a `LittleDict`
+    # that will hit a `MethodError` at the `Vector{UInt8}` constructor of `read(::S3Path)` if `raw=true`
+    # was not passed to `s3_get` in that method.
     s3_put(aws, bucket_name, "test_json", JSON3.write(my_dict), "application/json")
     json_bytes = read(json_path)
     @test JSON3.read(json_bytes, Dict) == my_dict
