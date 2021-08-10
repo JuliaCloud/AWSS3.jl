dad1ca582b89a0608f6009414a32060fdde7f57d
rofinn <rory.finnegan@gmail.com>
2021-08-10 17:56:25 -0500

    Try to reduce the need for the `isdir` fallback when processing walkpath objects, by inspecting the next element.

--- src/s3path.jl
+++ src/s3path.jl
@@ -226,16 +226,20 @@ end
 function FilePathsBase.walkpath(fp::S3Path; kwargs...)
     # Select objects with that prefix
     objects = s3_list_objects(get_config(fp), fp.bucket, fp.key; delimiter="")
+    root = joinpath(fp, "/")
 
     # Construct a new Channel using a recursive internal `_walkpath!` function
     return Channel(; ctype=typeof(fp), csize=128) do chnl
-        _walkpath!(fp, fp, Iterators.Stateful(objects), chnl; kwargs...)
+        _walkpath!(root, root, Iterators.Stateful(objects), chnl; kwargs...)
     end
 end
 
 function _walkpath!(
     root::S3Path, prefix::S3Path, objects, chnl; topdown=true, onerror=throw, kwargs...
 )
+    @assert root.isdirectory
+    @assert prefix.isdirectory
+
     while true
         try
             # Start by inspecting the next element
@@ -243,22 +247,37 @@ function _walkpath!(
 
             # Early exit condition if we've exhausted the iterator or just the current prefix.
             next === nothing && return nothing
-            startswith(next["Key"], prefix.key) || return nothing
 
             # Extract the non-root part of the key
             k = chop(next["Key"]; head=length(root.key), tail=0)
 
-            # Determine the next appropriate child path
-            # 1. Next is a direct descendant of the current prefix (ie: we have a prefix object)
-            # 2. Next is a distant descendant of the current prefix (ie: we don't have prefix objects)
             fp = joinpath(root, k)
             _parents = parents(fp)
-            child, recurse = if last(_parents) == prefix || fp.segments == prefix.segments
+
+            # If the filepath matches our prefix then pop it off and continue
+            # Cause we would have already processed it before recursing
+            child = if prefix.segments == fp.segments
                 popfirst!(objects)
-                fp, isdir(fp)
-            else
+                continue
+            # If the filpath is a direct descendant of our prefix then check if it
+            # is a directory too
+            elseif last(_parents) == prefix
+                popfirst!(objects)
+                next_child = Base.peek(objects)
+                if next_child !== nothing && startswith(next_child["Key"], fp.key)
+                    joinpath(fp, "/")
+                else
+                    joinpath(fp, isdir(fp) ? "/" : "")
+                end
+            # If our filepath is a distance descendant of the prefix then start filling in
+            # the intermediate paths
+            elseif prefix in _parents
                 i = findfirst(==(prefix), _parents)
-                _parents[i + 1], true
+                _parents[i + 1]
+            # Otherwise we've established that the current filepath isn't a descendant
+            # of the prefix and we should exit
+            else
+                return nothing
             end
 
             # If we aren't dealing with the root and we're doing topdown iteration then
@@ -266,7 +285,9 @@ function _walkpath!(
             !isempty(k) && topdown && put!(chnl, child)
 
             # Apply our recursive call for the children as necessary
-            if recurse
+            # NOTE: We're relying on the `isdirectory` field rather than calling `isdir`
+            # which will call out to AWS as a fallback.
+            if child.isdirectory
                 _walkpath!(
                     root, child, objects, chnl; topdown=topdown, onerror=onerror, kwargs...
                 )
