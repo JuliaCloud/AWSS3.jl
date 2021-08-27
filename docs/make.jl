cf9dcccdaaf8b87b2bd031ca2540f13467b6a718
Expanding Man <savastio@protonmail.com>
2021-08-27 13:07:13 -0400

    style fixes

--- docs/make.jl
+++ docs/make.jl
@@ -6,10 +6,10 @@ DocMeta.setdocmeta!(AWSS3, :DocTestSetup, :(using AWSS3); recursive=true)
 makedocs(;
     modules=[AWSS3],
     sitename="AWSS3.jl",
-    format=Documenter.HTML(
+    format=Documenter.HTML(;
         canonical="https://juliacloud.github.io/AWSS3.jl/stable/", edit_link="main"
     ),
-    pages=["Home" => "index.md", "API"=>"api.md"],
+    pages=["Home" => "index.md", "API" => "api.md"],
 )
 
 deploydocs(repo="github.com/JuliaCloud/AWSS3.jl.git",
