33cfaebbfbb26a843aa55fd75d23bc09fda11b80
ExpandingMan <ExpandingMan@users.noreply.github.com>
2021-09-18 11:48:12 -0400

    Update test/s3path.jl
    Co-authored-by: Eric Hanson <5846501+ericphanson@users.noreply.github.com>

--- test/s3path.jl
+++ test/s3path.jl
@@ -157,18 +157,20 @@ function test_s3_properties(ps::PathSet)
         @test fp2.key == "path/to/some/prefix/"
         @test fp2.version === nothing
 
-        fp3 = S3Path(ps.root.bucket, "/another/testdir/")
-        strs = ["what up", "what else up", "what up again"]
-        write(fp3 / "testfile1.txt", strs[1])
-        write(fp3 / "testfile2.txt", strs[2])
-        write(fp3 / "inner" / "testfile3.txt", strs[3])
-        @test AWSS3.diskusage(fp3) == sum(ncodeunits.(strs))
-
-        # we deliberately pick an older file to compare to so we
-        # can be confident timestamps are different
-        @test AWSS3.lastmodified(fp3) > AWSS3.lastmodified(ps.foo)
-
-        rm(S3Path(ps.root.bucket, "/another/"); recursive=true)  # otherwise subsequent tests may fail
+        try
+            fp3 = S3Path(ps.root.bucket, "/another/testdir/")
+            strs = ["what up", "what else up", "what up again"]
+            write(fp3 / "testfile1.txt", strs[1])
+            write(fp3 / "testfile2.txt", strs[2])
+            write(fp3 / "inner" / "testfile3.txt", strs[3])
+            @test AWSS3.diskusage(fp3) == sum(ncodeunits.(strs))
+    
+            # we deliberately pick an older file to compare to so we
+            # can be confident timestamps are different
+            @test AWSS3.lastmodified(fp3) > AWSS3.lastmodified(ps.foo)
+        finally
+            rm(S3Path(ps.root.bucket, "/another/"); recursive=true)  # otherwise subsequent tests may fail
+        end
     end
 end
 
