ebd54da3a580a9e2b2f3dabd29d28f70b0185d0a
ExpandingMan <savastio@protonmail.com>
2022-01-25 15:17:27 -0500

    fix tests

--- test/s3path.jl
+++ test/s3path.jl
@@ -245,19 +245,19 @@ end
 function verify_files(path::S3Path)
     @test readdir(path) == ["emptydir/", "subdir1/", "test_01.txt"]
     @test readdir(path; join=true) ==
-        [path / "emptydir/", path / "subdir1/", path / "test_01.txt"]
+        string.([path / "emptydir/", path / "subdir1/", path / "test_01.txt"])
     @test readdir(path / "emptydir/") == []
     @test readdir(path / "emptydir/"; join=true) == []
     @test readdir(path / "subdir1/") == ["subdir2/", "test_02.txt", "test_03.txt"]
-    @test readdir(path / "subdir1/"; join=true) == [
+    @test readdir(path / "subdir1/"; join=true) == string.([
         path / "subdir1/" / "subdir2/",
         path / "subdir1/" / "test_02.txt",
         path / "subdir1/" / "test_03.txt",
-    ]
+    ])
     @test readdir(path / "subdir1/subdir2/") == ["subdir3/", "test_04.txt"]
-    @test readdir(path / "subdir1/subdir2/"; join=true) == [
+    @test readdir(path / "subdir1/subdir2/"; join=true) == string.([
         path / "subdir1/subdir2/" / "subdir3/", path / "subdir1/subdir2/" / "test_04.txt"
-    ]
+    ])
     @test readdir(path / "subdir1/subdir2/subdir3/") == []
     @test readdir(path / "subdir1/subdir2/subdir3/"; join=true) == []
 end
@@ -269,11 +269,11 @@ function verify_files(path::AbstractPath)
     @test readdir(path / "emptydir/") == []
     VERSION >= v"1.4.0" && @test readdir(path / "emptydir/"; join=true) == []
     @test readdir(path / "subdir1/") == ["subdir2", "test_02.txt", "test_03.txt"]
-    VERSION >= v"1.4.0" && @test readdir(path / "subdir1/"; join=true) == [
+    VERSION >= v"1.4.0" && @test readdir(path / "subdir1/"; join=true) == string.([
         path / "subdir1" / "subdir2",
         path / "subdir1" / "test_02.txt",
         path / "subdir1/" / "subdir1/test_03.txt",
-    ]
+    ])
     @test readdir(path / "subdir1/subdir2/") == ["subdir3", "test_04.txt"]
     VERSION >= v"1.4.0" && @test readdir(path / "subdir1/subdir2/"; join=true) == [
         path / "subdir1/subdir2/" / "subdir3", path / "subdir1/subdir2/" / "test_04.txt"
