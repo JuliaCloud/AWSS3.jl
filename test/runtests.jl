7b98d74f66c67bea6bb8be2abba1ba9314a55343
Curtis Vogt <curtis.vogt@gmail.com>
2023-06-07 11:40:43 -0500

    Remove outdated VERSION checks

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ using FilePathsBase
 using FilePathsBase.TestPaths
 using FilePathsBase: /, join
 using JSON3
+using Minio
 using Mocking
 using Retry
 using Test
@@ -17,16 +18,14 @@ Mocking.activate()
 @service S3 use_response_type = true
 
 is_aws(config) = config isa AWSConfig
+AWS.aws_account_number(::Minio.MinioConfig) = "123"
 
 # Load the test functions
 include("s3path.jl") # creates `awss3_tests(config)`
 include("awss3.jl") # creates `s3path_tests(config)`
 
 @testset "AWSS3.jl" begin
-    if VERSION >= v"1.5"
-        using Minio
-        AWS.aws_account_number(::Minio.MinioConfig) = "123"
-
+    @testset "Minio" begin
         # We run most tests under Minio. This can be done locally by those
         # without access to the s3 bucket under which CI is performed.
         # We then run all tests with s3 directly.
@@ -42,19 +41,17 @@ include("awss3.jl") # creates `s3path_tests(config)`
                     "http://localhost:$port"; username="minioadmin", password="minioadmin"
                 ),
             )
-            @testset "Minio" begin
-                awss3_tests(config)
-                s3path_tests(config)
-            end
+            awss3_tests(config)
+            s3path_tests(config)
         finally
             # Make sure we kill the server even if a test failed.
             kill(minio_server)
         end
     end
 
-    # Set `AWSConfig` as the default for the following tests
-    aws = global_aws_config(AWSConfig())
     @testset "S3" begin
+        # Set `AWSConfig` as the default for the following tests
+        aws = global_aws_config(AWSConfig())
         awss3_tests(aws)
         s3path_tests(aws)
     end
